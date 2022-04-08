c687b748ab548ec879edd5863a1e34ef580339bd
Kevin From <kevinfrom@live.dk>
2022-04-08 22:15:26 +0200

    refactor: Remove debug

--- src/Controller/ListsController.php
+++ src/Controller/ListsController.php
@@ -30,7 +30,5 @@ class ListsController extends AppController implements ControllerInterface
      */
     public function index()
     {
-        $lists = $this->Lists->query()->where('name = My test list')->where(['id' => 2])->all();
-        dd($lists);
     }
 }
