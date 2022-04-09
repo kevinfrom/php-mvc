9d350513f71e2f7176c99e7a66667e97910d46eb
Kevin From <kevinfrom@live.dk>
2022-04-09 12:09:44 +0200

    feature: Added logging for errors in app requirements

--- config/requirements.php
+++ config/requirements.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Logging\Logger;
+
 $REQUIRED_PHP_VERSION = '8.1.0';
 if (version_compare(PHP_VERSION, $REQUIRED_PHP_VERSION) < 0) {
+    Logger::error('Installed PHP version ' . PHP_VERSION . ' is not great enough for ' . $REQUIRED_PHP_VERSION);
     trigger_error("Your PHP version must be equal or higher than $REQUIRED_PHP_VERSION.\n", E_USER_ERROR);
 }
 
@@ -9,6 +12,7 @@ $requiredExtensions = ['pdo_mysql', 'intl', 'json', 'mbstring'];
 
 foreach ($requiredExtensions as $extension) {
     if (extension_loaded($extension) === false) {
+        Logger::error("Required extension $extension is not enabled");
         trigger_error("You must enable the $extension extension." . PHP_EOL, E_USER_ERROR);
     }
 }
--- index.php
+++ index.php
@@ -3,13 +3,14 @@
  * Bootstrap application
  */
 
+require_once 'config' . DIRECTORY_SEPARATOR . 'paths.php';
+require_once 'vendor' . DS . 'autoload.php';
+
 ini_set('error_reporting', E_ALL);
 require_once 'config' . DIRECTORY_SEPARATOR . 'requirements.php';
 
 use App\Application\Application;
 
-require_once 'config' . DIRECTORY_SEPARATOR . 'paths.php';
-require_once 'vendor' . DS . 'autoload.php';
 require_once CONFIG . DS . 'functions.php';
 
 Application::getInstance()->bootstrap();
